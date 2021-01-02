29548b606e40a2d0c9c98e47fb3146e37d89acb8
Catarina Cândido <72628746+CatarinaCandido@users.noreply.github.com>
2021-01-02 18:31:54 +0000

    Table search working

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
+
 class Expense{
 
-    constructor(year, month, day, type, description, values) {
-        this.year = year
-        this.month = month
-        this.day = day
+    constructor(date, type, description, values) {
+        this.date = date
         this.type = type
         this.description = description
         this.values = values
@@ -12,12 +11,12 @@ class Expense{
     validate(){
 
         for (let i in this){
-         return (this[i] === '')
+         return (this[i] === '' || this[i] == null || this[i] === undefined )
         }
     }
 }
 
-class Dao{
+class DAO{
 
     constructor() {
         let id = localStorage.getItem('id')
@@ -26,31 +25,68 @@ class Dao{
             localStorage.setItem('id',0)
         }
     }
-
     save(expense){
         let ids = localStorage.getItem('id');
         ids++
-
         localStorage.setItem('id',ids)
         localStorage.setItem(ids,JSON.stringify(expense))
+    }
+
+    search(){
+
+        let id = localStorage.getItem('id')
+        let expenses = Array()
+
+        for (let i = 0; i <= id; i++) {
+            let expense = JSON.parse(localStorage.getItem(i))
+            expenses.push(expense)
+        }
+        return expenses
     }
 }
 
 
 function addCost(){
 
-    let year =document.getElementById('year').value
-    let month = document.getElementById('month').value
-    let day = document.getElementById('day').value
+    let date = document.getElementById('date').value
     let type = document.getElementById('type').value
     let description = document.getElementById('description').value
-    let values = document.getElementById('values').value
+    let values = document.getElementById('values').value + ' €'
 
-    let expense = new Expense(year, month, day, type, description, values)
-    let dao = new Dao()
+    let expense = new Expense(date, type, description, values)
+    let Dao = new DAO()
 
-    expense.validate()? alert('Errado') : alert('Correto')
-    dao.save(expense)
+    if(expense.validate() === true){
+        $('#errorMessage').modal('show')
+    }else if(expense.validate() === false){
+        $('#successMessage').modal('show')
+        Dao.save(expense)
+    }
+}
 
+function cleanForm(){
+    document.getElementById('date').value = ''
+    document.getElementById('type').value = ''
+    document.getElementById('description').value = ''
+    document.getElementById('values').value = ''
+}
+
+function displayExpenses(){
+
+    let Dao = new DAO()
+    let expenses = Dao.search()
+
+    var table = document.getElementById('tableExpenses')
+
+    expenses.forEach(function(){
+
+        for (let i = 1; i <= expenses.length; i++) {
+            var row = table.insertRow();
+            row.insertCell().innerHTML = expenses[i].date
+            row.insertCell().innerHTML = expenses[i].type
+            row.insertCell().innerHTML = expenses[i].description
+            row.insertCell().innerHTML = expenses[i].values
+        }
+    })
 }
 
